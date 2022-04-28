a760b4bf60a70a9781a8021352b919a908829935
Sanjeevrcs <103890473+Sanjeevrcs@users.noreply.github.com>
2022-04-29 00:19:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
-let addtodobutton = document.getElementById('addtodo');
-let todocontainer = document.getElementById('todocontainer');
-let inputfield = document.getElementById('inputfield');
+var addtodobutton = document.getElementById('addtodo');
+var todocontainer = document.getElementById('todocontainer');
+var inputfield = document.getElementById('inputfield');
 
 
 addtodobutton.addEventListener('click', function(){
     var fset=document.createElement('fieldset');
-    var paragraph = document.createElement('p');
+    var task=document.createElement('input');
+    var paragraph=document.createElement('p');
     var button=document.createElement('button');
+    var ebutton=document.createElement('button');
+
     button.classList.add('finishbutton');
     button.innerText='remove item';
+    ebutton.classList.add('finishbutton');
+    ebutton.innerText='edit task';
+    paragraph.appendChild(task);
     paragraph.classList.add('paragraph-styling');
+    task.classList.add('taskstyle');
+    
     let myvalue=inputfield.value;
-if(myvalue=="")
-{
-    alert("value cannot be empty");
-    return;
-}
-    paragraph.innerText = (inputfield.value)+"\t";
+    if(myvalue=="")
+    {
+        alert("task cannot be empty");
+        return;
+    }
+
+    task.type="text";
+    task.value=myvalue;
+    task.setAttribute("readonly","readonly");
+
     todocontainer.appendChild(paragraph);
     paragraph.appendChild(button);
+    paragraph.appendChild(ebutton);
 
-   
-    inputfield.value = "";
-   
+    inputfield.value="";
+
+    ebutton.addEventListener('click', function(){
+       if (ebutton.innerText=="edit task")
+       {
+           task.focus();
+           ebutton.innerText='save task';
+           task.removeAttribute("readonly");
+       }
+       else
+       {    if(task.value==""){
+           alert("task cannot be empty");
+       }
+       else{
+            task.setAttribute("readonly","readonly");
+            ebutton.innerText='edit task';   
+       }
+    }
+        
+    })
     button.addEventListener('click', function(){
         todocontainer.removeChild(paragraph);
         todocontainer.removeChild(button);
